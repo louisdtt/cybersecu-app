baf98a151acf2cde31b9fea699e6da8c1adeaea2
Louis Dutoit <louis.dutoitld@gmail.com>
2020-12-16 17:15:37 +0100

    file structure

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
+
+// Import routes
+let apiRoutes = require("./api-routes")// Use Api routes in the App
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use('/api', apiRoutes)
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
